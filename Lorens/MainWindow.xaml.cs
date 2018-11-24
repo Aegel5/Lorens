1f82092c8cfb4b8fea5ba3618e0c041a999af819
alex <alex@alex.ru>
2018-11-24 11:48:23 +0300

    improvment

--- Lorens/MainWindow.xaml.cs
+++ Lorens/MainWindow.xaml.cs
@@ -22,24 +22,18 @@ namespace Lorens
     {
         double xc;
         double yc;
-        Panel paintPanel = null;
+        double w;
+        double h;
+        Canvas paintPanel = null;
         public MainWindow()
         {
             InitializeComponent();
             paintPanel = canvas;
-            xc = paintPanel.Width / 2.0;
-            yc = paintPanel.Height / 2.0;
-            DrawAxes();
-        }
-
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+            
 
         }
 
-
+        double ykoeff = 1;
         double koeffScale = 1;
         double IncKoeff(double val)
         {
@@ -56,7 +50,7 @@ namespace Lorens
 
         double yToCanv(double y)
         {
-            return ((yc) - IncKoeff(y));
+            return ((yc) - IncKoeff(y)/ykoeff);
         }
 
         double xFromCanv(double x)
@@ -66,77 +60,187 @@ namespace Lorens
 
         double yFromCanv(double y)
         {
-            return DecKoeff(yc - y);
+            return DecKoeff(yc - y) * ykoeff;
         }
 
-        void wrapLine(Line l)
+        bool CheckPointInBorder(double xcanv, double ycanv)
         {
-            l.X1 = xToCanv(l.X1);
-            l.X2 = xToCanv(l.X2);
-            l.Y1 = yToCanv(l.Y1);
-            l.Y2 = yToCanv(l.Y2);
+            if (xcanv < 0 || xcanv > w || ycanv < 0 || ycanv > h)
+                return false;
+
+            return true;
         }
 
+
+        List<UIElement> axesElements = new List<UIElement>();
         void DrawAxes()
         {
-
-            var w = paintPanel.Width;
-            var h = paintPanel.Height;
-
-            var xc = w / 2;
-            var yc = h / 2;
-
-
-            Line line = new Line();
-
-            line.Stroke = new SolidColorBrush(Colors.Black);
-            line.StrokeThickness = 1;
-            line.X1 = -xc;
-            line.Y1 = 0;
-            line.X2 = xc;
-            line.Y2 = 0;
-
-            wrapLine(line);
-            paintPanel.Children.Add(line);
-
-            line = new Line();
-
-            line.Stroke = new SolidColorBrush(Colors.Black);
-            line.StrokeThickness = 1;
-            line.X1 = 0;
-            line.Y1 = -yc;
-            line.X2 = 0;
-            line.Y2 = yc;
-
-            wrapLine(line);
-            paintPanel.Children.Add(line);
-
-            var xpart = w / countAx;
-            var ypart = h / countAx;
-            for (int i = 0; i < countAx+1; i++)
+            void AddToPanel(UIElement el)
             {
+                paintPanel.Children.Add(el);
+                Canvas.SetZIndex(el, -1);
+                axesElements.Add(el);
+            }
+            w = paintPanel.ActualWidth;
+            h = paintPanel.ActualHeight;
+            xc = w / 2.0;
+            yc = h / 2.0;
+
+            foreach (var el in axesElements)
+            {
+                paintPanel.Children.Remove(el);
+            }
+            axesElements.Clear();
+
+            void DrawSimple()
+            {
+                Line line = new Line();
+
+
+
+                line.Stroke = new SolidColorBrush(Colors.Black);
+                line.StrokeThickness = 1;
+                line.X1 = 0;
+                line.Y1 = yc;
+                line.X2 = w;
+                line.Y2 = yc;
+                AddToPanel(line);
+
                 line = new Line();
 
-                line.Stroke = new SolidColorBrush(Colors.Gray);
-                line.StrokeThickness = 0.5;
-                line.X1 = 0 + xpart*i;
+                line.Stroke = new SolidColorBrush(Colors.Black);
+                line.StrokeThickness = 1;
+                line.X1 = xc;
                 line.Y1 = 0;
-                line.X2 = 0 + xpart * i;
+                line.X2 = xc;
                 line.Y2 = h;
-                paintPanel.Children.Add(line);
+                AddToPanel(line);
+
+                int countAx = 15;
+                var xpart = w / countAx;
+                var ypart = h / countAx;
+                var rpart = Math.Max(xpart, ypart);
+                for (int i = 0; i < countAx + 1; i++)
+                {
+                    line = new Line();
+
+                    line.Stroke = new SolidColorBrush(Colors.Gray);
+                    line.StrokeThickness = 0.5;
+                    line.X1 = 0 + rpart * i;
+                    line.Y1 = 0;
+                    line.X2 = 0 + rpart * i;
+                    line.Y2 = h;
+                    AddToPanel(line);
+                }
+
+                for (int i = 0; i < countAx + 1; i++)
+                {
+                    line = new Line();
+
+                    line.Stroke = new SolidColorBrush(Colors.Gray);
+                    line.StrokeThickness = 0.5;
+                    line.X1 = 0;
+                    line.Y1 = 0 + rpart * i;
+                    line.X2 = w;
+                    line.Y2 = 0 + rpart * i;
+                    //if (!CheckPointInBorder(line.X1, line.Y1) || !CheckPointInBorder(line.X2, line.Y2))
+                    //    continue;
+                    AddToPanel(line);
+                }
             }
 
-            for (int i = 0; i < countAx + 1; i++)
-            {
-                line = new Line();
+            DrawSimple();
 
-                line.Stroke = new SolidColorBrush(Colors.Gray);
-                line.StrokeThickness = 0.5;
-                line.X1 = 0;
-                line.Y1 = 0 + ypart * i;
-                line.X2 = w;
-                line.Y2 = 0 + ypart * i;
-                paintPanel.Children.Add(line);
+
+
+            // единичные окружности
+            void draw(double rad, bool drawY)
+            {
+                void draw1(bool invertX)
+                {
+                    void draw2(bool invertY)
+                    {
+                        PointCollection polygonPoints = new PointCollection();
+                        void dd(double x, double y)
+                        {
+                            if (invertY)
+                                y = -y;
+                            if (invertX)
+                                x = -x;
+
+                            if(drawY)
+                            {
+                                (x,y)=(y,x);
+                            }
+
+                            double xcanv = xToCanv(x);
+                            double ycanv = yToCanv(y);
+
+                            if (!CheckPointInBorder(xcanv, ycanv))
+                                return;
+
+                            polygonPoints.Add(new Point(xcanv, ycanv));
+                        }
+                        double xstart = rad;
+                        double xend = xFromCanv(w);
+                        double inc = 0;
+                        if (drawY)
+                        {
+                            xend = yFromCanv(0);
+                        }
+                        int countp = 100;
+                        inc = (xend - xstart) / countp;
+                        double rad2 = rad * rad;
+                        for (double i = 0; i <= countp; i++)
+                        {
+                            double x = xstart + i * inc;
+                            if (i == countp)
+                                x = xend;
+                            if (x < rad)
+                                continue;
+                            double y = Math.Sqrt(x*x - rad2);
+
+                            dd(x, y);
+                        }
+
+                        // рассчитаем последнюю видимую точку
+                        double yend = yFromCanv(0);
+                        if (drawY)
+                        {
+                            yend = xFromCanv(w);
+                        }
+                        xend = Math.Sqrt(yend * yend + rad2);
+                        dd(xend, yend);
+
+
+                        Polyline yellowPolyline = new Polyline();
+                        yellowPolyline.Stroke = new SolidColorBrush(Colors.Gray);
+                        yellowPolyline.StrokeThickness = 0.5;
+                        yellowPolyline.Points = polygonPoints;
+
+                        AddToPanel(yellowPolyline);
+                    }
+
+                    draw2(true);
+                    draw2(false);
+                }
+
+                draw1(true);
+                draw1(false);
+            }
+
+            int hypcount = 15;
+            double radinc = Math.Max(w / hypcount, h / hypcount);
+            radinc /= koeffScale;
+
+            for (double i = radinc; i <= w; i += radinc)
+            {
+                draw(i, false);
+            }
+
+            for (double i = radinc * ykoeff; i <= h * ykoeff; i += radinc * ykoeff)
+            {
+                draw(i, true);
             }
 
         }
@@ -179,7 +283,6 @@ namespace Lorens
         const int prad = 8;
         private void canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            int k = 0;
             var cur = e.GetPosition((IInputElement)sender);
             if (addLineStep == AddLine.First)
             {
@@ -255,10 +358,12 @@ namespace Lorens
                 var ec = eStart;
                 Canvas.SetTop(ec, line.Y1 - ec.Height / 2);
                 Canvas.SetLeft(ec, line.X1 - ec.Width / 2);
+                Canvas.SetZIndex(ec, 10);
 
                 ec = eEnd;
                 Canvas.SetTop(ec, line.Y2 - ec.Height / 2);
                 Canvas.SetLeft(ec, line.X2 - ec.Width / 2);
+                Canvas.SetZIndex(ec, 10);
             }
         }
 
@@ -269,7 +374,7 @@ namespace Lorens
             Second
         }
 
-        int countAx = 20;
+
 
         MyLine lineToAdd = new MyLine();
 
@@ -285,13 +390,15 @@ namespace Lorens
 
         private void canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            if(addLineStep == AddLine.Second)
-            {
-                var curP = e.GetPosition((IInputElement)sender);
+            var curP = e.GetPosition((IInputElement)sender);
+            var x = xFromCanv(curP.X);
+            var y = yFromCanv(curP.Y);
 
+            if (addLineStep == AddLine.Second)
+            {
                 var line = lineToAdd.line;
-                lineToAdd.endPoint.X = xFromCanv(curP.X);
-                lineToAdd.endPoint.Y = yFromCanv(curP.Y);
+                lineToAdd.endPoint.X = x;
+                lineToAdd.endPoint.Y = y;
                 line.X2 = curP.X;
                 line.Y2 = curP.Y;
 
@@ -303,9 +410,27 @@ namespace Lorens
                 ec.Fill = Brushes.Yellow;
 
                 lineToAdd.AdjustEllipses();
+            }
+            else if(addLineStep == AddLine.Null)
+            {
+                if(e.MouseDevice.LeftButton == MouseButtonState.Pressed)
+                {
+                    if(selected != null)
+                    {
+                        var x1 = selected.firstPoint.X;
+                        var y1 = selected.firstPoint.Y;
 
-                //paintPanel.Children.Remove(lineToAdd);
-                //paintPanel.Children.Add(lineToAdd);
+                        var x2 = selected.endPoint.X;
+                        var y2 = selected.endPoint.Y;
+
+                        var v = (x - x2) / Math.Abs(y1 - y2);
+
+                        if(Math.Abs(v) > 0 && Math.Abs(v) < 1)
+                        {
+                            DoLorenc(-v);
+                        }
+                    }
+                }
             }
         }
         void DeleteSelected()
@@ -347,6 +472,34 @@ namespace Lorens
             Select(selected);
         }
 
+        void DoLorenc(double v)
+        {
+            if (selected == null)
+                return;
+
+            foreach (var el in lstLines)
+            {
+                var line = el.line;
+
+                var x1 = el.firstPoint.X;
+                var t1 = el.firstPoint.Y;
+                var x2 = el.endPoint.X;
+                var t2 = el.endPoint.Y;
+
+                var (x1new, t1new) = Utils.CalcLorens(x1, t1, v, 1);
+                var (x2new, t2new) = Utils.CalcLorens(x2, t2, v, 1);
+
+                el.firstPoint.X = (x1new);
+                el.firstPoint.Y = (t1new);
+                el.endPoint.X = (x2new);
+                el.endPoint.Y = (t2new);
+
+                ToCanvasKoord(el);
+            }
+
+            Select(selected);
+        }
+
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
             if (selected == null)
@@ -364,29 +517,7 @@ namespace Lorens
                 return;
             }
 
-            foreach (var el in lstLines)
-            {
-                var line = el.line;
-
-                var x1 = el.firstPoint.X;
-                var t1 = el.firstPoint.Y;
-                var x2 = el.endPoint.X;
-                var t2 = el.endPoint.Y;
-
-                var v = (x2 - x1) / (t2 - t1);
-
-                var (x1new, t1new) = Utils.CalcLorens(x1, t1, cur_v, 1);
-                var (x2new, t2new) = Utils.CalcLorens(x2, t2, cur_v, 1);
-
-                el.firstPoint.X = (x1new);
-                el.firstPoint.Y = (t1new);
-                el.endPoint.X = (x2new);
-                el.endPoint.Y = (t2new);
-
-                ToCanvasKoord(el);
-            }
-
-            Select(selected);
+            DoLorenc(cur_v);
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -409,20 +540,48 @@ namespace Lorens
             el.line.Y2 = yToCanv(el.endPoint.Y);
             el.AdjustEllipses();
         }
-
+        
         private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
         {
             addLineStep = AddLine.Null;
 
-            if (e.Delta > 0)
+            double kchange = Math.Abs(e.Delta / 100.0);
+
+            bool fCtrl = Keyboard.Modifiers == ModifierKeys.Control;
+
+                if (e.Delta > 0)
             {
-                koeffScale *= e.Delta / 100.0;
+                if (fCtrl)
+                {
+                    ykoeff *= kchange;
+                }
+                else
+                {
+                    koeffScale *= kchange;
+                }
             }
             else if (e.Delta < 0)
             {
-                koeffScale /= Math.Abs(e.Delta) / 100.0;
+                if (fCtrl)
+                {
+                    ykoeff /= kchange;
+                }
+                else
+                {
+                    koeffScale /= kchange;
+                }
             }
 
+            if(fCtrl)
+            {
+                DrawAxes();
+            }
+
+            DrawLines();
+        }
+
+        void DrawLines()
+        {
             foreach (var el in lstLines)
             {
                 ToCanvasKoord(el);
@@ -431,7 +590,13 @@ namespace Lorens
 
         private void canvas_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            // redraw axes
+            DrawAxes();
+            DrawLines();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DrawAxes();
         }
     }
 }
